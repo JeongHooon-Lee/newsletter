95009947910faae1531774507e48e5f4a5a4632a
JeongHoon Lee <sherry0_@kakao.com>
2024-03-25 02:18:58 +0900

    day1

---
+++ src/routes/subscriptions.rs
@@ -0,0 +1,55 @@
+use actix_web::{HttpResponse, web};
+use chrono::Utc;
+use sqlx::{ PgPool};
+use uuid::Uuid;
+use tracing::Instrument;
+
+#[derive(serde::Deserialize)]
+pub struct FormData {
+    email: String,
+    name: String,
+}
+
+pub async fn subscribe(
+    form: web::Form<FormData>,
+    pool: web::Data<PgPool>,
+) -> HttpResponse {
+    let request_id = Uuid::new_v4();
+
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",%request_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    );
+
+    let _request_span_guard = request_span.enter();
+
+    let query_span = tracing::info_span!(
+        "Saving new subscriber details in the database"
+    );
+
+    // log::info!("Adding '{}' '{}' as a new subscriber", form.email, form.name);
+    // log::info!("Saving new subscriber details in the database");
+
+    match sqlx::query!(
+        r#"
+        INSERT INTO subscriptions
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+        .execute(pool.get_ref())
+        .instrument(query_span)
+        .await  {
+        Ok(_) => {
+            HttpResponse::Ok().finish()
+        },
+        Err(e) => {
+            tracing::error!("Failed to execute query: {:?}", e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
+}
